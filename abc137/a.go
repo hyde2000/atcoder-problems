175202715064569c3b132612cd511e4ee0b275e0
hidetomo0102 <hidenorishio@icloud.com>
2022-01-15 10:19:50 +0900

    fix

--- abc137/a.go
+++ abc137/a.go
@@ -1,1 +1,22 @@
-package abc137
+package main
+
+import "fmt"
+
+func main() {
+	var A, B int
+	fmt.Scan(&A, &B)
+
+	plus := A + B
+	minus := A - B
+	times := A * B
+
+	max := plus
+	if minus > max {
+		max = minus
+	}
+	if times > max {
+		max = times
+	}
+
+	fmt.Println(max)
+}
