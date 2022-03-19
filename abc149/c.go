8420696a7c77d2434033aeecfd3ac7063d2d5085
hidetomo0102 <hidenorishio@icloud.com>
2022-03-19 10:33:17 +0900

    C - Next Prime

--- abc149/c.go
+++ abc149/c.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	var X int64
+	fmt.Scan(&X)
+
+	for {
+		if big.NewInt(X).ProbablyPrime(0) {
+			break
+		}
+		X++
+	}
+	fmt.Println(X)
+}
