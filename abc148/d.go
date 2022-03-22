5994aa824d1d1274359008e4f5aefd19ffe0f21e
hidetomo0102 <hidenorishio@icloud.com>
2022-03-22 10:37:17 +0900

    D - Brick Break

--- abc148/d.go
+++ abc148/d.go
@@ -1,1 +1,27 @@
 package main
+
+import "fmt"
+
+func main() {
+	var N int
+	fmt.Scan(&N)
+
+	idx := 1
+	ans := 0
+	for i := 1; i <= N; i++ {
+		var a int
+		fmt.Scan(&a)
+
+		if a == idx {
+			idx++
+		} else {
+			ans++
+		}
+	}
+
+	if idx == 1 {
+		fmt.Println(-1)
+	} else {
+		fmt.Println(ans)
+	}
+}
